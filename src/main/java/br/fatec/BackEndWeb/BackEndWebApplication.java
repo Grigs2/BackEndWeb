f1b401b476cca88df33b75caddfe8dc1a25a958d
Otavio Grigolon <otaviogrigolon@gmail.com>
2025-06-03 17:16:39 -0300

    Commit do projeto base Configuração do application.properties para conectar com o banco de dados

---
+++ src/main/java/br/fatec/BackEndWeb/BackEndWebApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.BackEndWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/BackEndWeb/BackEndWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.BackEndWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
